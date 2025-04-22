ac620461ef4094c5bafa0f4a5877d33a84558d9b
noahdark812 <titannoah812@gmail.com>
2025-04-22 14:00:07 +0300

    add all const

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pygame
-
+import random
 pygame.init()
 
 screen_width = 800
 screen_height = 600
-screen = pygame.display.setmode((screen_width, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-pygame.display.set_caption("Game tir")
-icon = pygame.image.load("")
+pygame.display.set_caption("MinecraftNoah")
+icon = pygame.image.load("img/Minecraft-creeper-face.jpg")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/zombi.png")
+target_width = 80
+target_height = 80
+
+target_x = random.randint(0, screen_width - target_width)
+target_y = random.randint(0, screen_height - target_height)
+
+color = (random.randint(0, 255),random.randint(0, 255), random.randint(0,255))
 
 
 
