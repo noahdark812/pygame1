3eef4e3ddebb89b61eb3602e7f14b4f70a384e33
noahdark812 <titannoah812@gmail.com>
2025-04-23 08:25:24 +0300

    add all cycle code

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("MinecraftNoah")
-icon = pygame.image.load("img/Minecraft-creeper-face.jpg")
+icon = pygame.image.load("img/avatar.png")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/zombi1.png")
+target_img = pygame.image.load("img/zombi.png")
 target_width = 80
 target_height = 80
 
@@ -23,7 +23,19 @@ color = (random.randint(0, 255),random.randint(0, 255), random.randint(0,255))
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, screen_width - target_width)
+                target_y = random.randint(0, screen_height - target_height)
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
+
+
 
 pygame.quit()
 
